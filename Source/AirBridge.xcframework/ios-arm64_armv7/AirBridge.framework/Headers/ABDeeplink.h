6187aee5e07e59c0fccc0f153b3ca891a9f9e0bd
Subeom Choi <subumm1@gmail.com>
2022-07-28 15:18:36 +0900

    [1.24.5] release

---
+++ Source/AirBridge.xcframework/ios-arm64_armv7/AirBridge.framework/Headers/ABDeeplink.h
@@ -0,0 +1,46 @@
+//
+//  ABDeeplink.h
+//  AirBridge
+//
+//  Created by WOF on 06/09/2019.
+//  Copyright © 2019 ab180. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <AirBridge/ABError.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ABDeeplink : NSObject
+
+/* handle deeplink */
+- (void) handleUniversalLink:(NSURL*)link;
+- (void) handleUniversalLink:(NSURL*)link
+                      onDone:(nullable void (^)(NSString* deeplink))onDone;
+- (void) handleUniversalLink:(NSURL*)link
+                      onDone:(nullable void (^)(NSString* deeplink))onDone
+                     onError:(nullable void (^)(NSError* error))onError;
+
+- (void) handleURLSchemeDeeplink:(NSURL*)url;
+- (void) handleURLSchemeDeeplink:(NSURL*)url
+                          onDone:(nullable void (^)(NSString* deeplink))onDone;
+
+- (void) handleNotificationDeeplink:(NSURL*)url;
+
+/* set callback */
+- (void) setDeeplinkCallback:(void (^)(NSString* deeplink))callback;
+- (void) setDeferredDeeplinkCallback:(void (^)(NSString* deeplink))callback;
+
+/* set option */
+- (void) setHandleTrackingLinkTimeout:(uint64_t)timeout;
+
+@end
+
+@interface ABDeeplinkError : ABError
+
++ (ABDeeplinkError*) noDeeplink;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/AirBridge.xcframework/ios-arm64_armv7/AirBridge.framework/Headers/ABProductKey.h
@@ -0,0 +1,20 @@
+//
+//  ABProductKey.h
+//  AirBridge
+//
+//  Created by WOF on 2020/09/25.
+//  Copyright © 2020 ab180. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABProductKey : NSObject
+
+@property (class, nonatomic, readonly) NSString* productID;
+@property (class, nonatomic, readonly) NSString* name;
+@property (class, nonatomic, readonly) NSString* price;
+@property (class, nonatomic, readonly) NSString* currency;
+@property (class, nonatomic, readonly) NSString* position;
+@property (class, nonatomic, readonly) NSString* quantity;
+
+@end
